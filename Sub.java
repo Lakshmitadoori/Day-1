c39fb7849ebe654594f419c2915df79f6f4c8a43
Lakshmi Prasanna <lakshmi.tadoori@zemosolabs.com>
2022-11-10 19:29:38 +0530

    modifies Sub.java

--- Sub.java
+++ Sub.java
@@ -2,6 +2,6 @@ public class Sub{
     public static void main(String args[]){
         int a=5;
         int b=10;
-        System.out.println(a-b);
+        System.out.println("The diff is"a-b);
     }
 }
